0c92de25d7326d2a93215794d3ef51d7ac09f067
Lance Newby <lnewby@gmail.com>
2023-02-24 21:18:35 -0800

    Add reference books data store.

---
+++ datastore/books.js
@@ -0,0 +1,95 @@
+export const books = {
+    dev: [
+        {
+           title: 'Game Engine Architecture',
+           author: 'Jason Gregory',
+           url: 'https://www.amazon.com/Engine-Architecture-Third-Jason-Gregory/dp/1138035459?crid=3SAFKTXDB7EKZ&keywords=game+engine+architecture+by+jason+gregory&qid=1677298470&sprefix=Game+Engine+Architecture%2Caps%2C872&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=e819857d3c23a53fd96521373ef065eb&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'Real-Time Collision Detection',
+            author: 'Christer Ericson',
+            url: 'https://www.amazon.com/Real-Time-Collision-Detection-Christer-Ericson-ebook/dp/B00CLZIKC2?crid=3NR0MM2N1JOVQ&keywords=Real-Time+Collision+Detection&qid=1677298736&sprefix=real-time+collision+detection%2Caps%2C180&sr=8-3&linkCode=ll1&tag=gdinterview-20&linkId=e2ed4d5b45f5e2f6495d37b2951f127d&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'Effective Modern C++',
+            author: 'Scott Meyer',
+            url: 'https://www.amazon.com/Effective-Modern-Specific-Ways-Improve/dp/1491903996?crid=2WWTOSTUXYTMC&keywords=Effective+Modern+C%2B%2B&qid=1677298866&sprefix=effective+modern+c%2B%2B%2Caps%2C202&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=adc61864210ae3db98c1574d7bc71c95&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'Rules of Play: Game Design Fundamentals',
+            author: 'Katie Salen Tekinbas and Eric Zimmerman',
+            url: 'https://www.amazon.com/Rules-Play-Game-Design-Fundamentals-ebook/dp/B08BT1BKZV?_encoding=UTF8&qid=1677298931&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=6c7c4dd4042c6a2712a1ca8a4fd53a03&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'Game Programming Patterns',
+            author: 'Robert Nystrom',
+            url: 'https://www.amazon.com/Game-Programming-Patterns-Robert-Nystrom-ebook/dp/B00P5URD96?_encoding=UTF8&qid=1677299017&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=f4d4c91a2f4e5829dd4bc7247ce285ba&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'Programming Game AI by Example',
+            author: 'Mat Buckland',
+            url: 'https://www.amazon.com/Programming-Example-Wordware-Developers-Library-ebook/dp/B0029LCJXE?_encoding=UTF8&qid=1677299282&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=7737a6ecb525b0d8abe3bb4e0a8698f4&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+            author: 'Robert C. Martin',
+            url: 'https://www.amazon.com/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882?crid=269HLHZXI90FK&keywords=clean+code&qid=1677299129&sprefix=clean+cod%2Caps%2C200&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=9930722ad02e6c692bf3ea350f522b77&language=en_US&ref_=as_li_ss_tl', 
+        },
+    ],
+    design: [
+        {
+            title: 'Rules of Play: Game Design Fundamentals',
+            author: 'Katie Salen Tekinbas and Eric Zimmerman',
+            url: 'https://www.amazon.com/Rules-Play-Game-Design-Fundamentals-ebook/dp/B08BT1BKZV?_encoding=UTF8&qid=1677298931&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=6c7c4dd4042c6a2712a1ca8a4fd53a03&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'Flow: The Psychology of Optimal Experience',
+            author: 'Mihaly Csikszentmihalyi',
+            url: 'https://www.amazon.com/Flow-Psychology-Experience-Perennial-Classics/dp/0061339202?crid=QRDO22NDEJIA&keywords=flow+mihaly+csikszentmihalyi&qid=1677299223&sprefix=Flow+miha%2Caps%2C179&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=cb32de3503f0fff641537b857c171f14&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'The Art of Game Design',
+            author: 'Jesse Schell',
+            url: 'https://www.amazon.com/Art-Game-Design-Lenses-Third-ebook/dp/B07X59RN6N?_encoding=UTF8&qid=1677299330&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=c708f8f962d8075946470cfec142c795&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'A Theory of Fun for Game Design',
+            author: 'Raph Koster',
+            url: 'https://www.amazon.com/Theory-Game-Design-Raph-Koster-ebook/dp/B00GK5SRFY?_encoding=UTF8&qid=1677299421&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=3fa157b8d8b266ecad74923a11d58fa3&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'Level Up! The Guide to Great Video Game Design - 2nd Edition',
+            author: 'Scott Rogers ',
+            url: 'https://www.amazon.com/Level-Guide-Great-Video-Design-ebook/dp/B00JRYDCEQ?_encoding=UTF8&qid=1677299456&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=88f3a57f05ff3390a4f591ae206870d7&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'Game Mechanics: Advanced Game Design',
+            author: 'Ernest Adams and Joris Dormans',
+            url: 'https://www.amazon.com/Game-Mechanics-Advanced-Design-Voices-ebook/dp/B008CG8E8Y?crid=13Q3HICRZUO69&keywords=Game+Mechanics%3A+Advanced+Game+Design&qid=1677299568&sprefix=game+mechanics+advanced+game+design%2Caps%2C277&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=67ae1fc3a4d1cc45f924718bd84b1d96&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'Game Feel',
+            author: 'Steve Swink',
+            url: 'https://www.amazon.com/Game-Feel-Designers-Sensation-Kaufmann-ebook/dp/B07FDBQKF6?_encoding=UTF8&qid=1677299614&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=a82af0e2d8e67dcf503128b088401ba0&language=en_US&ref_=as_li_ss_tl', 
+        },
+        {
+            title: 'Game Design Workshop',
+            author: 'Tracy Fullerton',
+            url: 'https://www.amazon.com/Game-Design-Workshop-Playcentric-Innovative/dp/1138098779?crid=S5FO90D0G8I4&keywords=Game+Design+Workshop&qid=1677299678&sprefix=game+design+workshop%2Caps%2C281&sr=8-1&ufe=app_do%3Aamzn1.fos.c1e7b9a2-53cf-44f5-ab39-5386af17669d&linkCode=ll1&tag=gdinterview-20&linkId=60dc1c7d6659ba08f5389bf6bdfa45c7&language=en_US&ref_=as_li_ss_tl', 
+        },
+    ],
+    general: [
+        {
+            title: 'Creativity, Inc.',
+            author: 'Amy Wallace and Edwin Catmull',
+            url: 'https://www.amazon.com/Creativity-Inc-Overcoming-Unseen-Inspiration/dp/0812993012?crid=15K7C0U13LH9I&keywords=Creativity%2C+Inc.&qid=1677299503&sprefix=creativity%2C+inc.%2Caps%2C145&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=7887b751585e9503d06c39aef0b820fe&language=en_US&ref_=as_li_ss_tl', 
+        },
+    ],
+    math: [
+        {
+            title: ' Foundations of Game Engine Development, Volume 1: Mathematics',
+            author: 'Eric Lengyel',
+            url: 'https://www.amazon.com/Foundations-Game-Engine-Development-Mathematics-ebook/dp/B084CT8H46?_encoding=UTF8&qid=1677299374&sr=8-1&linkCode=ll1&tag=gdinterview-20&linkId=9a0c2909235533a6e89b68f194019ee7&language=en_US&ref_=as_li_ss_tl', 
+        },
+    ],
+};
